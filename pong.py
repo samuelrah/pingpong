98596e5e911da19f2b1f8a0a855072fbf883b7e5
samuelrah <samuel.rahseparmohammadi@elev.ga.ntig.se>
2025-05-07 16:03:48 +0200

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,2 +1,13 @@
-gg
-gg 2
+import turtle
+import time
+
+# Skapa fönster
+win = turtle.Screen()
+win.title("Pong med Turtle")
+win.bgcolor("black")
+win.setup(width=800, height=600)
+win.tracer(0)
+
+# Poäng
+score_a = 0
+score_b = 0
